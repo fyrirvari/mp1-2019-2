3f9eeb0a11e1c076c0584a20be37480a7a58b24b
fyrirvari <klevtsov.n@mail.ru>
2019-09-16 20:27:12 +0300

    Added a program

---
+++ klevtsov_nm/task0/Source.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+void main()
+{
+	double x1, y1, R1, x2, y2, R2, d, branch;
+	printf("First (x, y), R: ");
+	scanf_s("%lf %lf %lf", &x1, &y1, &R1);
+
+	printf("Second (x, y), R: ");
+	scanf_s("%lf %lf %lf", &x2, &y2, &R2);
+	
+	d = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+	//Окружность с данными x1, y1, R1 должна быть больше чем вторая.
+	if (R2 > R1)
+	{
+		branch = x2; x2 = x1; x1 = branch;
+		branch = y2; y2 = y1; y1 = branch;
+		branch = R2; R2 = R1; R1 = branch;
+	}
+
+	//Определяем взаимное расположение двух окружностей.
+	if ((R1 == R2) && (x1 == x2) && (y1 == y2))
+	{
+		printf("Окружности совпадают.");
+	}
+	else if (R1 + R2 < d)
+	{
+		printf("Не имеют общих точек.");
+	}
+	else if ((R1 + R2 == d) || (R1 - R2 == d))
+	{
+		printf("Окружности соприкасаются.");
+	}
+	else if ((R1 + R2 > d) && (R1 - R2 < d))
+	{
+		printf("Окружности пересекаются.");
+	}
+	else if (R1 - R2 > d)
+	{
+		printf("Окружность с меньшим радиусом внутри другой.");
+	}
+	_getch();
+}
