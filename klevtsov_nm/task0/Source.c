4e79e1130e609c2a7f8c079837d06249980ea6c1
fyrirvari <klevtsov.n@mail.ru>
2019-09-16 22:44:24 +0300

    Added a program on C instead C++ xD

--- klevtsov_nm/task0/Source.c
+++ klevtsov_nm/task0/Source.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 #include <conio.h>
+#include <locale.h>
 
 void main()
 {
+	setlocale(LC_ALL, "Rus");
+
 	double x1, y1, R1, x2, y2, R2, d, branch;
-	printf("First (x, y), R: ");
+	printf("Введите значения первой окружности (x, y), R: ");
 	scanf_s("%lf %lf %lf", &x1, &y1, &R1);
 
-	printf("Second (x, y), R: ");
+	printf("Введите значения второй окружности (x, y), R: ");
 	scanf_s("%lf %lf %lf", &x2, &y2, &R2);
 	
 	d = sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
@@ -16,9 +19,9 @@ void main()
 	//Окружность с данными x1, y1, R1 должна быть больше чем вторая.
 	if (R2 > R1)
 	{
-		branch = x2; x2 = x1; x1 = branch;
-		branch = y2; y2 = y1; y1 = branch;
-		branch = R2; R2 = R1; R1 = branch;
+		branch = R2;
+		R2 = R1;
+		R1 = branch;
 	}
 
 	//Определяем взаимное расположение двух окружностей.
