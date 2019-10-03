ba17f9fae0f83e29da0721dda68d1547e84cd83a
fyrirvari <klevtsov.n@mail.ru>
2019-10-03 17:22:42 +0300

    Task2 + Versus c:

--- klevtsov_nm/task2/Source.c
+++ klevtsov_nm/task2/Source.c
@@ -2,15 +2,119 @@
 #include <time.h>
 #include <locale.h>
 
+//Пользователь против компьютера.
+void userPcVersus()
+{
+	int userAlleged, userQt;
+
+	int pcGuessed, pcAlleged, pcQt;
+	int lLim, rLim;
+	char symb;
+
+	lLim = 1; rLim = 1000;
+	pcQt = userQt = 0;
+
+	pcGuessed = rand() % 1000 + 1;
+
+
+	//Инициализация локальной переменной "userAlleged".
+	printf("Введите число от 0 до 1000: ");
+	scanf_s("%d", &userAlleged);
+
+	if (userAlleged < pcGuessed)
+		printf("Загаданное число больше.\n");
+	else if (userAlleged > pcGuessed)
+		printf("Загаданное число меньше.\n");
+	else
+		printf("Вы угадали.\n");
+
+	userQt++;
+
+
+	//Инициализация локальной переменной "symb".
+	pcAlleged = (lLim + rLim) / 2;
+
+	printf("Это %d?(Введите, если загаданное больше: \">\", меньше: \"<\", равно: \"=\")\n", pcAlleged);
+	getchar(); scanf_s("%c", &symb);
+
+
+	if (symb == '>')
+	{
+		lLim = pcAlleged;
+	}
+	else if (symb == '<')
+	{
+		rLim = pcAlleged;
+	}
+	else
+	{
+		printf("Число угаданно. ");
+	}
+
+	pcQt++;
+
+
+	while ((symb != '=') || (pcGuessed != userAlleged))
+	{
+		if (pcGuessed != userAlleged);
+		{
+			printf("Введите число от 0 до 1000: ");
+			scanf_s("%d", &userAlleged);
+
+			if (userAlleged < pcGuessed)
+				printf("Загаданное число больше.\n");
+			else if (userAlleged > pcGuessed)
+				printf("Загаданное число меньше.\n");
+			else
+				printf("Вы угадали.\n");
+
+			userQt++;
+		}
+
+
+		if (symb != '=')
+		{
+			pcAlleged = (lLim + rLim) / 2;
+
+			printf("Это %d?(Введите, если загаданное больше: \">\", меньше: \"<\", равно: \"=\")\n", pcAlleged);
+			getchar(); scanf_s("%c", &symb);
+
+
+			if (symb == '>')
+			{
+				lLim = pcAlleged;
+			}
+			else if (symb == '<')
+			{
+				rLim = pcAlleged;
+			}
+			else
+			{
+				printf("Число угаданно.\n");
+			}
+
+			pcQt++;
+		}
+	}
+
+
+	if (pcQt < userQt)
+		printf("Победил компьютер, угадав число за %d ходов.\n", pcQt);
+	else if (pcQt > userQt)
+		printf("Победил пользователь, угадав число за %d ходов.\n", userQt);
+	else
+		printf("Как говорится: \"Победила дружба\".\n");
+}
+
 //Пользователь отгадывает.
-int userGuesses()
+void userGuesses()
 {
 	int  guessed, alleged, qt;
 	qt = 0;
 
 	guessed = rand() % 1000 + 1;
 
-	//Пользователь пытается отгдать загаданное компьютером число.
+
 	do
 	{
 		printf("Введите число от 0 до 1000: ");
@@ -26,13 +130,14 @@ int userGuesses()
 		qt++;
 	} while (alleged != guessed);
 
-	return qt;
+
+	printf("Количество попыток - %d.\n", qt);
 }
 
 //Компьютер отгадывает.
-int computerGuesses()
+void computerGuesses()
 {
-	int  guessed, alleged, qt;
+	int alleged, qt;
 	int lLim, rLim;
 	char symb;
 
@@ -43,9 +148,9 @@ int computerGuesses()
 	{
 		alleged = (lLim + rLim) / 2;
 
-		//Пользователь сообщает компьютеры в каких отношениях предлагаемое число с загаданным.
-		printf("Это %d?(Загаданное больше: >, меньше: <, равно: =)\n", alleged);
+		printf("Это %d?(Введите, если загаданное больше: \">\", меньше: \"<\", равно: \"=\")\n", alleged);
 		getchar(); scanf_s("%c", &symb);
+
 
 		if (symb == '>')
 		{
@@ -63,41 +168,46 @@ int computerGuesses()
 		qt++;
 	} while (symb != '=');
 
-	return qt;
+	printf("Количество попыток - %d.\n", qt);
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 
-	int variable, qt;
+	int variable;
 	
 	srand(time(NULL));
+
 
 	//Выбор режима программы "Угадай число".
 	printf("Выберите режим программы:\n");
 	printf("0)Выход.\n");
 	printf("1)Режим №1 (компьютер загадывает число).\n");
 	printf("2)Режим №2 (пользователь загадывает число).\n");
+	printf("2)Режим №3 (кто быстрее пользователь или компьютер).\n");
 	scanf_s("%d", &variable);
 	
+
 	switch (variable)
 	{
 	case 0:
 		exit();
 		break;
 	case 1:
-		qt = userGuesses();
-		printf("Количество попыток - %d.\n", qt);
+		userGuesses();
 		break;
 	case 2:
-		qt = computerGuesses();
-		printf("Количество попыток - %d.\n", qt);
+		computerGuesses();
+		break;
+	case 3:
+		userPcVersus();
 		break;
 	default:
 		printf("Не выбран режим.\n");
 		break;
 	} 
+
 
 	printf("Попробовать снова?\n");
 	printf("1)Да.\n");
