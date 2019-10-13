8f8bd0dc14c49be346d56e94ab9832db997390f9
fyrirvari <klevtsov.n@mail.ru>
2019-10-13 16:05:29 +0300

    Added stop word c:

--- klevtsov_nm/task3/Source.c
+++ klevtsov_nm/task3/Source.c
@@ -1,6 +1,8 @@
 ﻿#include <stdio.h>
 #include <time.h>
 #include <locale.h>
+#include <math.h>
+#include <string.h>
 #define True 1
 
 //Получение цифры неповторяющейся с предыдущими.
@@ -39,10 +41,56 @@ int are_there_duplicates(int alleged[5], int n)
 	return 0;
 }
 
+//Проверка на принадлежность, состоящего из цифр от 1 до 9, множеству.
+int is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+//Проверка символа на пренадлежность к алфавиту.
+int is_word(char c)
+
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+//Введено ни число, ни стоп-слово - "0", введено стоп-слово - "-1", введено не n-значное число - "-2"
+int get_entered(n)
+{
+	int result = 0, k = n;
+	char c[20];
+	int l_lim = 0, r_lim = 0; //Диапазон, в которых должно находится введенное число.
+	gets(c);
+	if (is_digit(c[0])) //Проверка на цифру.
+	{
+		for (int i = 0; i < k; i++) //Считаем, что введено число, если в строке первые n элементов - цифры.
+		{
+			if (is_digit(c[i]))
+			{
+				result += (c[i] - '0') * pow(10, --n);
+				l_lim += pow(10, n);
+				r_lim += 9 * pow(10, n);
+			}
+			else
+				return 0;
+		}
+		if (result >= l_lim && result <= r_lim)
+			return result;
+		else
+			return -2;
+	}
+	else if (!(strcmp(c, "stop")))
+	{
+		return -1;
+	}
+	else
+		return 0;
+}
+
 void bulls_and_cows(int n)
 {
 	int alleged[5], guessed[5];
-	int entered, bulls, cows, cnt = 0;
+	int entered, bulls, cows, cnt = 0, flag = 1;
 
 	//Компьютер "задумывает" n-значное число с неповторяющимися цифрами.
 	guessed[0] = rand() % 9 + 1;
@@ -54,10 +102,26 @@ void bulls_and_cows(int n)
 	//Вычисление "коров" и "быков".
 	while (True)
 	{
-		cnt++;
 		bulls = cows = 0;
-		printf("Введите %d-значное число с неповторяющимися цифрами: ", n);
-		scanf_s("%d", &entered);
+		printf("Введите %d-значное число с неповторяющимися цифрами или \"stop\" для того, чтобы сдаться.\n", n);
+		entered = get_entered(n);
+		if (entered == -1)
+		{
+			printf("Вы сдались.\n");
+			flag = 0;
+			break;
+		}
+		else if (entered == 0)
+		{
+			printf("Неверный ввод. Попробуйте еще раз.\n");
+			continue;
+		}
+		else if (entered == -2)
+		{
+			printf("Введено не %d-значное число. Попробуйте еще раз.\n", n);
+			continue;
+		}
+
 		for (int i = n - 1; i >= 0; i--)
 		{
 			alleged[i] = entered % 10;
@@ -83,8 +147,10 @@ void bulls_and_cows(int n)
 			break;
 		else
 			printf("Быков: %d.\nКоров: %d.\n", bulls, cows);
+		cnt++;
 	}
-	printf("Вы угадали за %d попыток! Моё уважение.\n", cnt);
+	if (flag)
+		printf("Вы угадали за %d попыток! Моё уважение.\n", cnt);
 }
 
 //Выбор длины загадываемого числа.
@@ -92,7 +158,7 @@ void set_length()
 {
 	int n;
 	printf("Выберите длину загадываемого числа(от 2 до 5), если же хотите выйти из программы введите \"0\": ");
-	scanf_s("%d", &n);
+	scanf_s("%d", &n); getchar();
 	switch (n)
 	{
 	case 0:
